42e7cd511f86be8fedc845cd43a07088a7403036
Ghazaligh <ghazalibasharmuhammad>
2026-03-19 09:31:46 +0100

    Fininshed Day 1 of Python challenge

---
+++ day_01_of_30days_of_python.py
@@ -0,0 +1,25 @@
+# Introduction
+# Day 1 - 30DaysOfPython Challenge
+
+
+#Day 1 - 30DaysOfPython Challenge
+print(3+2) #addition(+)
+print(3-2) #subtraction(-)
+print(3*2) #multiplication(*)
+print(3/2) #division(/)
+print(3**2) #exponentiation(**)
+print(3%2) #modulus(%)
+print(3//2) #floor division(//)
+
+print(type(10))                  # Int
+print(type(3.14))                # Float
+print(type(1 + 3j))              # Complex
+print(type('Asabeneh'))          # String
+print(type([1, 2, 3]))           # List
+print(type({'name':'Asabeneh'})) # Dictionary
+print(type({9.8, 3.14, 2.7}))    # Set
+print(type((9.8, 3.14, 2.7)))    # Tuple
+print(type(3 == 3))              # Bool
+print(type(3 >= 3))              # Bool
+result = 3 > 2
+print(result)                    # True
